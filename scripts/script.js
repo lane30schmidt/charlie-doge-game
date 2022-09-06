c088a5be1c8a3d9853e47bdacbaa2fce1877b8a6
lane30schmidt <111534250+lane30schmidt@users.noreply.github.com>
2022-09-06 14:36:51 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  //click on an image, get a message
+
+  $("#dogeOne").click(function () {
+    $("#outputDiv").hide();
+
+    console.log("user clicked dogeOne 2");
+    $("#outputMsg").text("You Lose, pick the REAL doge.");
+    $("#outputDiv").toggle();
+  });
+
+  $("#dogeTwo").click(function () {
+    $("#outputDiv").hide();
+    console.log("user clicked dogeTwo 2");
+    $("#outputMsg").text("YOU WIN, YOU HAS THE WOW!");
+    $("#outputDiv").toggle();
+  });
+});
